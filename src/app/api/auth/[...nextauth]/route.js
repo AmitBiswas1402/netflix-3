5627c651e79613f81da91b186e7ce00bdf2a9a3c
AmitBiswas1402 <amit.142biswas@gmail.com>
2024-11-12 12:26:22 +0530

    auth provider added

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,28 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: "27fa4d029b740a0b35fb",
+      clientSecret: "bc7de352584a8dbde7856d5468ba4e2a7aa35ac3",
+    }),
+  ],
+  callbacks: {
+    async session({ session, token, user }) {
+      session.user.username = session?.user?.name
+        .split(" ")
+        .join("")
+        .toLocaleLowerCase();
+
+      session.user.uid = token.sub;
+
+      return session;
+    },
+  },
+  secret: "default_secret_key",
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
