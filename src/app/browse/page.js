2d8cd790eafc2f727328a9fa10cb3f208b796059
AmitBiswas1402 <amit.142biswas@gmail.com>
2024-11-13 21:32:57 +0530

    manage accounts making

--- src/app/browse/page.js
+++ src/app/browse/page.js
@@ -1,16 +1,19 @@
 'use client'
 
+import { ManageAccounts } from "@/components/manage-accounts";
+import { GlobalContext } from "@/context";
+import { useContext } from "react";
 import UnauthPage from "@/components/unauth-page";
 import { useSession } from "next-auth/react"
 
 export default function Search() {
+    const {loggedInAccount} = useContext(GlobalContext)
 
     const {data: session} = useSession()
     console.log(session);
 
-    if (session === null) {
-        return <UnauthPage />
-    }
+    if (session === null) return <UnauthPage />
+    if (loggedInAccount === null) return <ManageAccounts />
     
     return (
         <>
---
+++ src/components/manage-accounts/index.js
@@ -0,0 +1,13 @@
+'use client'
+
+export const ManageAccounts = () => {
+  return (
+    <div className="min-h-screen flex justify-center flex-col items-center relative">
+        <div className="flex justify-center flex-col items-center">
+            <h1 className="text-white font-bold text-[54px] my-[36px]">
+
+            </h1>
+        </div>        
+    </div>
+  )
+}
