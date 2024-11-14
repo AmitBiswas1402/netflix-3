6f5a8d62944e8f4add53818c3957114506c33abc
AmitBiswas1402 <amit.142biswas@gmail.com>
2024-11-14 22:17:20 +0530

    motion

--- src/components/manage-accounts/account-form.js
+++ src/components/manage-accounts/account-form.js
@@ -1,2 +1,16 @@
 'use client'
+import {motion} from 'framer-motion'
 
+export default function AccountForm({}) {
+  return (
+    <motion.div initial={{opacity : 0}}
+    whileInView={{opacity : 1}}
+    viewport={{once : true}}
+    >
+        <div className=''>
+
+        </div>
+
+    </motion.div>
+  )
+}
